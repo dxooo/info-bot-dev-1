85f308f157200692cafe853c018a6864e63d850c
Raymond2112 <41760656+Raymond2112@users.noreply.github.com>
2018-07-27 15:56:47 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -206,7 +206,7 @@ async def searchpc(ctx, *, arg):
 
     embed = discord.Embed(title="__San Andreas Penal Code Lookup__", description=("It looks like you searched for "+us_inpt.title()), color=0x3D59AB)
     embed.set_author(name="State Police Info Bot", icon_url="https://cdn.discordapp.com/attachments/393324031505465344/471855906699739136/sasp_logo_updated_2018.png")
-    embed.add_field(name="Here's what we found based on your search:", value=', '.join(found_list)+\n, inline=False)
+    embed.add_field(name="Here's what we found based on your search:", value=', '.join(found_list)*'\n', inline=False)
     embed23 = await ctx.send(embed=embed)
     await asyncio.sleep(10) 
     await ctx.message.delete()
