8b13825d3d59386df974f316f0a82046b7bbc3ac
Raymond2112 <41760656+Raymond2112@users.noreply.github.com>
2018-07-27 16:34:41 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -272,7 +272,7 @@ async def searchvc(ctx, *, arg):
 
     embed = discord.Embed(title="__San Andreas Vehicle Code Lookup__", description=("It looks like you searched for "+us_inpt.title()), color=0x3D59AB)
     embed.set_author(name="State Police Info Bot", icon_url="https://cdn.discordapp.com/attachments/393324031505465344/471855906699739136/sasp_logo_updated_2018.png")
-    embed.add_field(name="Here's what we found based on your search:", value=', '.join(found_list), inline=False)
+    embed.add_field(name="Here's what we found based on your search:", value='\n'.join(found_list), inline=False)
     await ctx.send(embed=embed)
     await asyncio.sleep(10) 
     await ctx.message.delete()
